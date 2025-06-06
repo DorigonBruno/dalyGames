7b477096bc83fcbe7db2211545c0904e4c11a173
Bruno Dorigon <bdorigon@gmail.com>
2025-06-06 09:48:33 -0300

    build header

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IoGameController } from "react-icons/io5";
+
+export function Header() {
+  return (
+    <header className="bg-gray-300 w-full py-4 px-2">
+      <nav className="flex items-center justify-between w-full max-w-screen-xl m-auto">
+        <div className="flex items-center gap-10">
+          <Link href={"/"}>
+            <Image
+              src={"./logo.svg"}
+              alt="logo do site"
+              width={184}
+              height={40}
+            />
+          </Link>
+
+          <div className="hidden sm:flex gap-6">
+            <Link href={"/game"} className="font-bold sm:text-lg">
+              Jogos
+            </Link>
+            <Link href={"/profile"} className="font-bold sm:text-lg">
+              Perfil
+            </Link>
+          </div>
+        </div>
+
+        <button>
+          <IoGameController size={32} color="#475569" />
+        </button>
+      </nav>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-export default function Home () {
+export default function Home() {
   return (
-
     <div>
-      <h1 className="font-bold bg-green-300">Olá</h1>
+      <h1>Olá</h1>
     </div>
-  )
+  );
 }
