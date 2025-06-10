b1c86d3f8f267283aeb035e4005b79680ff20653
Bruno Dorigon <bdorigon@gmail.com>
2025-06-10 16:47:22 -0300

    fix config user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,10 +4,10 @@ import { FaShareAlt } from "react-icons/fa";
 
 export default function profile() {
   return (
-    <section className="w-full mt-6">
+    <section className="w-full mt-10 sm:mt-20">
       <Container>
-        <div className="flex justify-between">
-          <div className="flex items-center gap-4">
+        <div className="flex flex-col sm:flex-row justify-center items-center sm:justify-between gap-4">
+          <div className="flex flex-col sm:flex-row items-center gap-4">
             <Image
               src="/user.png"
               alt="Foto do usuário"
@@ -18,9 +18,11 @@ export default function profile() {
               className="object-cover block rounded-full w-42 h-42"
             />
 
-            <h1 className="font-bold text-xl text-gray-800">Bruno Dev</h1>
+            <h1 className="font-bold text-xl sm:text-5xl text-gray-800">
+              Bruno Dev
+            </h1>
           </div>
-          <div className="flex self-start gap-3">
+          <div className="flex sm:self-start gap-3">
             <button className="bg-gray-700 text-white px-3 py-2 rounded-lg cursor-pointer font-medium">
               Configurações
             </button>
